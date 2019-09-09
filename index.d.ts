cb5721146cfa4fad166abb42bf5baf2d82ddbc58
Reinhold Früsmer <rfruesmer@gmail.com>
2019-09-09 10:38:43 +0200

    Updated creation of index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export * from "./dist/deferred";
+export * from "./dist/message-header";
+export * from "./dist/message-queue";
+export * from "./dist/message-service";
+export * from "./dist/message-topic";
+export * from "./dist/message";
