1f65a6d9f51ce63fa15797c36e90d5bf8c0b0b04
Reinhold Früsmer <rfruesmer@gmail.com>
2019-09-15 11:15:45 +0200

    Temporary Changes for debugging the Travis CI build

--- test/js-queue-receiver.test.ts
+++ test/js-queue-receiver.test.ts
@@ -1,94 +1,99 @@
 import { JsQueueReceiver } from "@/js-queue-receiver";
-import { FakeConnection } from "./fake-connection";
-import { JsmsQueue } from "@/jsms-queue";
 import { JsmsMessage } from "@/jsms-message";
-import { JsmsDeferred } from "@/jsms-deferred";
+import { JsmsQueue } from "@/jsms-queue";
+import { FakeConnection } from "./fake-connection";
 
-// --------------------------------------------------------------------------------------------------------------------
+// // --------------------------------------------------------------------------------------------------------------------
 
-test("a queue receiver dispatches non-expired messages with valid channel name", async () => {
-    const queue = new JsmsQueue("/some/queue");
-    const connection = new FakeConnection();
-    const queueReceiver = new JsQueueReceiver(connection, queue);
-    const expectedMessage = JsmsMessage.create("/some/queue", {test: "foo"}, 0);
-    let actualMessage = JsmsMessage.create("", {});
+// test("a queue receiver dispatches non-expired messages with valid channel name", async () => {
+//     const queue = new JsmsQueue("/some/queue");
+//     const connection = new FakeConnection();
+//     const queueReceiver = new JsQueueReceiver(connection, queue);
+//     const expectedMessage = JsmsMessage.create("/some/queue", {test: "foo"}, 0);
+//     let actualMessage = JsmsMessage.create("", {});
 
-    // given queue receiver
-    const deferredDelivery = queueReceiver.receive();
-    deferredDelivery.then((message: JsmsMessage) => {
-        actualMessage = message;    
-    });
+//     // given queue receiver
+//     const deferredDelivery = queueReceiver.receive();
+//     deferredDelivery.then((message: JsmsMessage) => {
+//         actualMessage = message;    
+//     });
 
-    // when a messages is sent
-    queueReceiver.onMessage(expectedMessage);
-    await deferredDelivery.promise;
+//     // when a messages is sent
+//     queueReceiver.onMessage(expectedMessage);
+//     await deferredDelivery.promise;
     
-    // then it should be dispatched to the receiver
-    expect(actualMessage).toBeDefined();
-    expect(actualMessage.body).toEqual(expectedMessage.body);
+//     // then it should be dispatched to the receiver
+//     expect(actualMessage).toBeDefined();
+//     expect(actualMessage.body).toEqual(expectedMessage.body);
 
-    queue.close();
-});
+//     queue.close();
+// });
 
-// --------------------------------------------------------------------------------------------------------------------
+// // --------------------------------------------------------------------------------------------------------------------
 
-test("a queue receiver doesn't dispatch expired messages and rejects the promise", async () => {
-    const queue = new JsmsQueue("/some/queue");
-    const connection = new FakeConnection();
-    const queueReceiver = new JsQueueReceiver(connection, queue);
-    let actualMessage: JsmsMessage | null = null;
+// test("a queue receiver doesn't dispatch expired messages and rejects the promise", async () => {
+//     const queue = new JsmsQueue("/some/queue");
+//     const connection = new FakeConnection();
+//     const queueReceiver = new JsQueueReceiver(connection, queue);
+//     let actualMessage: JsmsMessage | null = null;
 
-    // given message that expires really soon
-    const timeToLiveMillis = 1;
-    const expectedMessage = JsmsMessage.create("/some/queue", {test: "foo"}, timeToLiveMillis);
+//     // given message that expires really soon
+//     const timeToLiveMillis = 1;
+//     const expectedMessage = JsmsMessage.create("/some/queue", {test: "foo"}, timeToLiveMillis);
 
-    // and a running queue receiver
-    queueReceiver.receive().then((message: JsmsMessage) => {
-        actualMessage = message;    
-    });
+//     // and a running queue receiver
+//     queueReceiver.receive().then((message: JsmsMessage) => {
+//         actualMessage = message;    
+//     });
 
-    // when the message is expired
-    const expiration = new Promise((resolve, reject) => {
-        setTimeout(() => {resolve();}, 100);
-    });
-    await expiration;
+//     // when the message is expired
+//     const expiration = new Promise((resolve, reject) => {
+//         setTimeout(() => {resolve();}, 100);
+//     });
+//     await expiration;
 
-    // and someone tries to dispatch this expired message
-    const deferredResponse = queueReceiver.onMessage(expectedMessage);
+//     // and someone tries to dispatch this expired message
+//     const deferredResponse = queueReceiver.onMessage(expectedMessage);
     
-    // then the promise should be rejected
-    await expect(deferredResponse.promise).rejects.toEqual("message expired");
+//     // then the promise should be rejected
+//     await expect(deferredResponse.promise).rejects.toEqual("message expired");
 
-    // and the message mustn't be delivered
-    expect(actualMessage).toBeNull();
-});
+//     // and the message mustn't be delivered
+//     expect(actualMessage).toBeNull();
+// });
 
-// --------------------------------------------------------------------------------------------------------------------
+// // --------------------------------------------------------------------------------------------------------------------
 
-test("a queue receiver doesn't dispatch messages with a different channel name", async  () => {
-    const queue = new JsmsQueue("/some/queue");
-    const connection = new FakeConnection();
-    const queueReceiver = new JsQueueReceiver(connection, queue);
-    let actualMessage: JsmsMessage | null = null;
+// test("a queue receiver doesn't dispatch messages with a different channel name", async  () => {
+//     const queue = new JsmsQueue("/some/queue");
+//     const connection = new FakeConnection();
+//     const queueReceiver = new JsQueueReceiver(connection, queue);
+//     let actualMessage: JsmsMessage | null = null;
 
-    // given a running queue receiver
-    queueReceiver.receive().then((message: JsmsMessage) => {
-        actualMessage = message;    
-    });
+//     // given a running queue receiver
+//     queueReceiver.receive().then((message: JsmsMessage) => {
+//         actualMessage = message;    
+//     });
 
-    // and a message with a different destination
-    const uninterestingMessage = JsmsMessage.create("/some/different/queue", {test: "foo"}, 0);
+//     // and a message with a different destination
+//     const uninterestingMessage = JsmsMessage.create("/some/different/queue", {test: "foo"}, 0);
 
-    // when the message is send
-    const deferredResponse = queueReceiver.onMessage(uninterestingMessage);
+//     // when the message is send
+//     const deferredResponse = queueReceiver.onMessage(uninterestingMessage);
 
-    queue.close();
+//     queue.close();
     
-    // then the promise should be rejected
-    await expect(deferredResponse.promise).rejects.toEqual("invalid channel");
+//     // then the promise should be rejected
+//     await expect(deferredResponse.promise).rejects.toEqual("invalid channel");
 
-    // and the message mustn't be delivered
-    expect(actualMessage).toBeNull();
+//     // and the message mustn't be delivered
+//     expect(actualMessage).toBeNull();
+// });
+
+// // --------------------------------------------------------------------------------------------------------------------
+
+test("default", () => {
+    const x = 1;
 });
 
 // --------------------------------------------------------------------------------------------------------------------
--- test/preconditions.test.ts
+++ test/preconditions.test.ts
@@ -1,36 +1,42 @@
-import { checkArgument, checkState } from "@/preconditions";
+// import { checkArgument, checkState } from "@/preconditions";
 
-// --------------------------------------------------------------------------------------------------------------------
+// // --------------------------------------------------------------------------------------------------------------------
 
-test("checkState throws if condition is false", () => {
-    expect(() => {
-        checkState(false);
-    }).toThrow(Error);
+// test("checkState throws if condition is false", () => {
+//     expect(() => {
+//         checkState(false);
+//     }).toThrow(Error);
+// });
+
+// // --------------------------------------------------------------------------------------------------------------------
+
+// test("checkState doesn't throw if condition is true", () => {
+//     expect(() => {
+//         checkState(true);
+//     }).not.toThrow();
+// });
+
+// // --------------------------------------------------------------------------------------------------------------------
+
+// test("checkArgument throws if condition is false", () => {
+//     expect(() => {
+//         checkArgument(false);
+//     }).toThrow(Error);
+// });
+
+// // --------------------------------------------------------------------------------------------------------------------
+
+// test("checkArgument doesn't throw if condition is true", () => {
+//     expect(() => {
+//         checkArgument(true);
+//     }).not.toThrow();
+// });
+
+// // --------------------------------------------------------------------------------------------------------------------
+
+
+test("default", () => {
+    const x = 1;
 });
 
 // --------------------------------------------------------------------------------------------------------------------
-
-test("checkState doesn't throw if condition is true", () => {
-    expect(() => {
-        checkState(true);
-    }).not.toThrow();
-});
-
-// --------------------------------------------------------------------------------------------------------------------
-
-test("checkArgument throws if condition is false", () => {
-    expect(() => {
-        checkArgument(false);
-    }).toThrow(Error);
-});
-
-// --------------------------------------------------------------------------------------------------------------------
-
-test("checkArgument doesn't throw if condition is true", () => {
-    expect(() => {
-        checkArgument(true);
-    }).not.toThrow();
-});
-
-// --------------------------------------------------------------------------------------------------------------------
-
