94f5a44d6a58dba35466827aa6b49292dfaa54d1
Reinhold Früsmer <rfruesmer@gmail.com>
2019-09-15 11:32:40 +0200

    Temporary Changes for debugging the Travis CI build

--- test/jsms-deferred.test.ts
+++ test/jsms-deferred.test.ts
@@ -1,109 +1,103 @@
-// import { JsmsDeferred } from "@/jsms-deferred";
-// import { getLogger, Logger } from "@log4js-node/log4js-api";
+import { JsmsDeferred } from "@/jsms-deferred";
+import { getLogger, Logger } from "@log4js-node/log4js-api";
 
-// let logger: Logger;
+let logger: Logger;
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// beforeAll(() => {
-//     logger = getLogger("jsms");
-//     logger.level = "debug";
-// });
+beforeAll(() => {
+    logger = getLogger("jsms");
+    logger.level = "debug";
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("can be resolved", async () => {
-//     const deferred = new JsmsDeferred<string>();
-//     deferred.resolve("test");
-//     await expect(deferred.promise).resolves.toEqual("test");
-// });
+test("can be resolved", async () => {
+    const deferred = new JsmsDeferred<string>();
+    deferred.resolve("test");
+    await expect(deferred.promise).resolves.toEqual("test");
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("can be rejected", async () => {
-//     const expectedError = new Error("fail");
-//     const deferred = new JsmsDeferred<string>();
-//     deferred.reject(expectedError);
-//     await expect(deferred.promise).rejects.toThrowError(expectedError);
-// });
+test("can be rejected", async () => {
+    const expectedError = new Error("fail");
+    const deferred = new JsmsDeferred<string>();
+    deferred.reject(expectedError);
+    await expect(deferred.promise).rejects.toThrowError(expectedError);
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("supports simple then callback", async () => {
-//     let actualResolvedValue = "";
-//     const expectedResolvedValue = "test";
+test("supports simple then callback", async () => {
+    let actualResolvedValue = "";
+    const expectedResolvedValue = "test";
 
-//     const deferred = new JsmsDeferred<string>();
-//     deferred.then((value: string) => {
-//         actualResolvedValue = value;
-//     });
+    const deferred = new JsmsDeferred<string>();
+    deferred.then((value: string) => {
+        actualResolvedValue = value;
+    });
 
-//     deferred.resolve(expectedResolvedValue);
-//     await deferred.promise;
+    deferred.resolve(expectedResolvedValue);
+    await deferred.promise;
 
-//     expect(actualResolvedValue).toEqual(expectedResolvedValue);
-// });
+    expect(actualResolvedValue).toEqual(expectedResolvedValue);
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("is resolved after catching an error", async () => {
-//     const deferred = new JsmsDeferred<string>();
-//     deferred.then((value: string) => {
-//         throw new Error("fail");
-//     });
+test("is resolved after catching an error", async () => {
+    const deferred = new JsmsDeferred<string>();
+    deferred.then((value: string) => {
+        throw new Error("fail");
+    });
 
-//     deferred.resolve("test");
-//     await expect(deferred.promise).resolves.toEqual("test");
-// });
+    deferred.resolve("test");
+    await expect(deferred.promise).resolves.toEqual("test");
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("supports chaining thens", async () => {
-//     let result = "";
+test("supports chaining thens", async () => {
+    let result = "";
 
-//     const promise = new Promise<string>((done) => {
-//         const d1 = new JsmsDeferred<string>();
-//         d1.then((value: string) => {
-//             return "r1";
-//         })
-//         .then((value: string) => {
-//             result = value;
-//             done(value);
-//         });
+    const promise = new Promise<string>((done) => {
+        const d1 = new JsmsDeferred<string>();
+        d1.then((value: string) => {
+            return "r1";
+        })
+        .then((value: string) => {
+            result = value;
+            done(value);
+        });
 
-//         d1.resolve("v1");
-//     });
+        d1.resolve("v1");
+    });
 
     
-//     await promise;
-//     expect(result).toEqual("r1");
-// });
+    await promise;
+    expect(result).toEqual("r1");
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("supports chaining resolves", async () => {
-//     let result = "";
+test("supports chaining resolves", async () => {
+    let result = "";
 
-//     const d1 = new JsmsDeferred<string>();
-//     d1.then((value: string) => {
-//         return "r1";
-//     });
+    const d1 = new JsmsDeferred<string>();
+    d1.then((value: string) => {
+        return "r1";
+    });
 
-//     const promise = new Promise<string>((done) => {
-//         d1.resolve("v1").then((value: string) => {
-//             result = value;
-//             done(value);
-//         });
-//     });
+    const promise = new Promise<string>((done) => {
+        d1.resolve("v1").then((value: string) => {
+            result = value;
+            done(value);
+        });
+    });
     
-//     await promise;
-//     expect(result).toEqual("r1");
-// });
-
-// // --------------------------------------------------------------------------------------------------------------------
-
-test("default", () => {
-    const x = 1;
+    await promise;
+    expect(result).toEqual("r1");
 });
 
 // --------------------------------------------------------------------------------------------------------------------
