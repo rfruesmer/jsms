188c7f15974f4a300e5bfe0d23773d9503aaafe6
Reinhold Früsmer <rfruesmer@gmail.com>
2019-09-15 11:20:26 +0200

    Temporary Changes for debugging the Travis CI build

--- test/preconditions.test.ts
+++ test/preconditions.test.ts
@@ -1,42 +1,35 @@
-// import { checkArgument, checkState } from "@/preconditions";
+import { checkArgument, checkState } from "@/preconditions";
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("checkState throws if condition is false", () => {
-//     expect(() => {
-//         checkState(false);
-//     }).toThrow(Error);
-// });
+test("checkState throws if condition is false", () => {
+    expect(() => {
+        checkState(false);
+    }).toThrow(Error);
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("checkState doesn't throw if condition is true", () => {
-//     expect(() => {
-//         checkState(true);
-//     }).not.toThrow();
-// });
+test("checkState doesn't throw if condition is true", () => {
+    expect(() => {
+        checkState(true);
+    }).not.toThrow();
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("checkArgument throws if condition is false", () => {
-//     expect(() => {
-//         checkArgument(false);
-//     }).toThrow(Error);
-// });
+test("checkArgument throws if condition is false", () => {
+    expect(() => {
+        checkArgument(false);
+    }).toThrow(Error);
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("checkArgument doesn't throw if condition is true", () => {
-//     expect(() => {
-//         checkArgument(true);
-//     }).not.toThrow();
-// });
-
-// // --------------------------------------------------------------------------------------------------------------------
-
-
-test("default", () => {
-    const x = 1;
+test("checkArgument doesn't throw if condition is true", () => {
+    expect(() => {
+        checkArgument(true);
+    }).not.toThrow();
 });
 
 // --------------------------------------------------------------------------------------------------------------------
