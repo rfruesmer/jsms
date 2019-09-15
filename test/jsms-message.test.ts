0746f6754fc1f3c234b01e58617bb0ecd53b48ff
Reinhold Früsmer <rfruesmer@gmail.com>
2019-09-15 11:41:46 +0200

    Temporary Changes for debugging the Travis CI build

--- test/jsms-message.test.ts
+++ test/jsms-message.test.ts
@@ -1,25 +1,19 @@
-// import { JsmsMessage } from "@/jsms-message";
+import { JsmsMessage } from "@/jsms-message";
 
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("creates default body", () => {
-//     const message = JsmsMessage.create("/some/channel");
-//     expect(message.body).toEqual({});
-// });
+test("creates default body", () => {
+    const message = JsmsMessage.create("/some/channel");
+    expect(message.body).toEqual({});
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("creates default response body", () => {
-//     const message = JsmsMessage.create("/some/channel");
-//     const responseMessage = JsmsMessage.createResponse(message);
-//     expect(responseMessage.body).toEqual({});
-// });
-
-// // --------------------------------------------------------------------------------------------------------------------
-
-test("default", () => {
-    const x = 1;
+test("creates default response body", () => {
+    const message = JsmsMessage.create("/some/channel");
+    const responseMessage = JsmsMessage.createResponse(message);
+    expect(responseMessage.body).toEqual({});
 });
 
 // --------------------------------------------------------------------------------------------------------------------
--- test/jsms-queue-message-expiration.test.ts
+++ test/jsms-queue-message-expiration.test.ts
@@ -1,89 +1,83 @@
-// import { JsmsService } from "@/jsms-service";
-// import { JsmsMessage } from "@/jsms-message";
-// import { FakeConnection } from "./fake-connection";
+import { JsmsService } from "@/jsms-service";
+import { JsmsMessage } from "@/jsms-message";
+import { FakeConnection } from "./fake-connection";
 
-// let messageService: JsmsService;
+let messageService: JsmsService;
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// beforeEach(() => {
-//     messageService = new JsmsService();
-// });
+beforeEach(() => {
+    messageService = new JsmsService();
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// afterEach(() => {
-//     // release resources
-//     messageService.close();
-// });
+afterEach(() => {
+    // release resources
+    messageService.close();
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("a queue doesn't deliver messages that expire inbetween sending and receiving", async () => {
-//     const queueName = "/some/queue";
-//     const messageBody = { test: "foo" };
-//     let receivedExpiredMessage = false;
+test("a queue doesn't deliver messages that expire inbetween sending and receiving", async () => {
+    const queueName = "/some/queue";
+    const messageBody = { test: "foo" };
+    let receivedExpiredMessage = false;
 
-//     // given the message is sent before the receiver is running and not expired yet 
-//     const timeToLiveMillis = 100;
-//     const deferredResponse = messageService.send(queueName, messageBody, timeToLiveMillis);
+    // given the message is sent before the receiver is running and not expired yet 
+    const timeToLiveMillis = 100;
+    const deferredResponse = messageService.send(queueName, messageBody, timeToLiveMillis);
 
-//     // and the message expires
-//     const expiration = new Promise((resolve, reject) => {
-//         setTimeout(() => { resolve(); }, 150);
-//     });
-//     await expiration;
+    // and the message expires
+    const expiration = new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(); }, 150);
+    });
+    await expiration;
     
-//     // when receiving the next message
-//     messageService.receive(queueName)
-//         .then((message: JsmsMessage) => {
-//             receivedExpiredMessage = true;
-//         });
+    // when receiving the next message
+    messageService.receive(queueName)
+        .then((message: JsmsMessage) => {
+            receivedExpiredMessage = true;
+        });
 
-//     // then the delivery should be rejected
-//     await expect(deferredResponse.promise).rejects.toEqual("message expired");
+    // then the delivery should be rejected
+    await expect(deferredResponse.promise).rejects.toEqual("message expired");
 
-//     // and the message shouldn't be delivered
-//     expect(receivedExpiredMessage).toBeFalsy();
-// });
+    // and the message shouldn't be delivered
+    expect(receivedExpiredMessage).toBeFalsy();
+});
 
-// // --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-// test("a queue doesn't deliver messages that are expired before being send", async () => {
-//     const queueName = "/some/queue";
-//     const connection = new FakeConnection();
-//     const queue = messageService.createQueue(queueName, connection);
-//     const producer = connection.getProducer(queue);
-//     let receivedExpiredMessage = false;
+test("a queue doesn't deliver messages that are expired before being send", async () => {
+    const queueName = "/some/queue";
+    const connection = new FakeConnection();
+    const queue = messageService.createQueue(queueName, connection);
+    const producer = connection.getProducer(queue);
+    let receivedExpiredMessage = false;
 
-//     // given a receiver is already running 
-//     messageService.receive(queueName)
-//         .then((message: JsmsMessage) => {
-//             receivedExpiredMessage = true;
-//         });
+    // given a receiver is already running 
+    messageService.receive(queueName)
+        .then((message: JsmsMessage) => {
+            receivedExpiredMessage = true;
+        });
 
-//     // and a message that is expired before being send
-//     const timeToLiveMillis = 1;
-//     const expiredMessage = JsmsMessage.create(queueName, { test: "foo" }, timeToLiveMillis);
-//     const expiration = new Promise((resolve, reject) => {
-//         setTimeout(() => {resolve();}, 10);
-//     });
-//     await expiration;
+    // and a message that is expired before being send
+    const timeToLiveMillis = 1;
+    const expiredMessage = JsmsMessage.create(queueName, { test: "foo" }, timeToLiveMillis);
+    const expiration = new Promise((resolve, reject) => {
+        setTimeout(() => {resolve();}, 10);
+    });
+    await expiration;
 
-//     // when trying to send the message
-//     const deferredResponse = producer.send(expiredMessage);
+    // when trying to send the message
+    const deferredResponse = producer.send(expiredMessage);
         
-//     // then the delivery should be rejected
-//     await expect(deferredResponse.promise).rejects.toEqual("message expired");
+    // then the delivery should be rejected
+    await expect(deferredResponse.promise).rejects.toEqual("message expired");
 
-//     // and the message shouldn't be delivered
-//     expect(receivedExpiredMessage).toBeFalsy();
-// });
-
-// // --------------------------------------------------------------------------------------------------------------------
-
-test("default", () => {
-    const x = 1;
+    // and the message shouldn't be delivered
+    expect(receivedExpiredMessage).toBeFalsy();
 });
 
 // --------------------------------------------------------------------------------------------------------------------
